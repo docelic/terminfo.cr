8da5ab223c4ea8649a7d5bb30892c12ee4fa049b
Benoit de Chezelles <lesell_b@epitech.eu>
2017-09-22 14:55:32 +0200

    Add basic EOFError handling (throw our exception)

--- src/terminfo.cr
+++ src/terminfo.cr
@@ -1,6 +1,16 @@
 require "./terminfo/*"
 
 module Terminfo
+  class Error < Exception
+  end
+
+  class FileTooShort < Error
+    getter db : String
+
+    def initialize(@message, @db)
+    end
+  end
+
   def self.parse!
     unless term_name = ENV["TERM"]?
       raise "No terminal set in environment variable TERM."
@@ -10,6 +20,10 @@ module Terminfo
       raise "terminfo database found for terminal #{term_name}"
     end
 
-    parse_terminfo db_path
+    begin
+      parse_terminfo db_path
+    rescue ex : IO::EOFError
+      raise FileTooShort.new "Unexpected end of file #{db_path.inspect}", db: db_path
+    end
   end
 end
