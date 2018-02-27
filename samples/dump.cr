11199263f41e38600bc5f80cdde228bc7e0aae85
Benoit de Chezelles <lesell_b@epitech.eu>
2018-02-26 19:33:54 -0800

    Fix samples/dump.cr

--- samples/dump.cr
+++ samples/dump.cr
@@ -16,21 +16,21 @@ puts
 
 puts "--> Terminfo bools:"
 ti.booleans.each_key do |idx|
-  key = Terminfo::KeyNames::Booleans[idx][:full]
+  key = Terminfo::KeyNames::Booleans[idx.value][:full]
   puts "  #{key}"
 end
 puts
 
 puts "--> Terminfo numbers:"
 ti.numbers.each do |idx, value|
-  key = Terminfo::KeyNames::Numbers[idx][:full]
+  key = Terminfo::KeyNames::Numbers[idx.value][:full]
   puts "  #{key} = #{value}"
 end
 puts
 
 puts "--> Terminfo strings:"
 ti.strings.each do |idx, bytes|
-  key = Terminfo::KeyNames::Strings[idx][:full]
+  key = Terminfo::KeyNames::Strings[idx.value][:full]
   puts "  #{key} = #{String.new(bytes).inspect}"
 end
 
