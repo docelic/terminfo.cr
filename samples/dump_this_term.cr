2e59545d958c0e8e4b770b2536cc2f24d25c2c17
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2020-05-10 09:51:13 +0200

    Use a dedicated Entry struct to describe full/short names

--- samples/dump_this_term.cr
+++ samples/dump_this_term.cr
@@ -19,20 +19,20 @@ puts
 
 puts "--> Terminfo bools:"
 ti.booleans.each_key do |idx|
-  key = Terminfo::KeyNames::Booleans[idx.value][:full]
+  key = Terminfo::KeyNames::Booleans[idx.value].full
   puts "  #{key}"
 end
 puts
 
 puts "--> Terminfo numbers:"
 ti.numbers.each do |idx, value|
-  key = Terminfo::KeyNames::Numbers[idx.value][:full]
+  key = Terminfo::KeyNames::Numbers[idx.value].full
   puts "  #{key} = #{value}"
 end
 puts
 
 puts "--> Terminfo strings:"
 ti.strings.each do |idx, bytes|
-  key = Terminfo::KeyNames::Strings[idx.value][:full]
+  key = Terminfo::KeyNames::Strings[idx.value].full
   puts "  #{key} = #{String.new(bytes).inspect}"
 end
