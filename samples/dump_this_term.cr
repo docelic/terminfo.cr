43e669094d3575eefd6e2e4befa21a935e1552c1
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2020-05-12 14:17:58 +0200

    sample: Simplify dump_this_term & sort strings capabilities

--- samples/dump_this_term.cr
+++ samples/dump_this_term.cr
@@ -17,22 +17,20 @@ ti.names.each do |name|
 end
 puts
 
-puts "--> Terminfo bools:"
-ti.booleans.each_key do |idx|
-  key = Terminfo::KeyNames::Booleans[idx.value].full
-  puts "  #{key}"
+puts "--> Terminfo boolean capabilities:"
+ti.booleans.each_key do |bool_cap|
+  puts "  #{bool_cap.long_name}"
 end
 puts
 
-puts "--> Terminfo numbers:"
-ti.numbers.each do |idx, value|
-  key = Terminfo::KeyNames::Numbers[idx.value].full
-  puts "  #{key} = #{value}"
+puts "--> Terminfo numeric capabilities:"
+ti.numbers.each do |num_cap, value|
+  puts "  #{num_cap.long_name} = #{value}"
 end
 puts
 
-puts "--> Terminfo strings:"
-ti.strings.each do |idx, bytes|
-  key = Terminfo::KeyNames::Strings[idx.value].full
-  puts "  #{key} = #{String.new(bytes).inspect}"
+puts "--> Terminfo strings capabilities (sorted alphabetically):"
+ti.strings.keys.sort_by(&.long_name).each do |str_cap|
+  bytes = ti.strings[str_cap]
+  puts "  #{str_cap.long_name} = #{String.new(bytes).inspect}"
 end
